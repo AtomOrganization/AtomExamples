b4b771545d09ff2daab6ffed5bd8eaec27504596
AdrienDev <73515932+Adrien35240@users.noreply.github.com>
2024-08-10 01:49:46 +0200

    add poc for c#

---
+++ c#/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+  [DllImport("ATOM.dll", CallingConvention = CallingConvention.Cdecl)]
+  public static extern IntPtr CreateAtom();
+
+  [DllImport("ATOM.dll", CallingConvention = CallingConvention.Cdecl)]
+  public static extern void DestroyAtom(IntPtr atom);
+
+  [DllImport("ATOM.dll", CallingConvention = CallingConvention.Cdecl)]
+  public static extern void InitializeAtom(IntPtr atom);
+
+  [DllImport("ATOM.dll", CallingConvention = CallingConvention.Cdecl)]
+  public static extern void DisplayMessageAtom(IntPtr atom, string message);
+
+  static void Main()
+  {
+    IntPtr atom = CreateAtom();
+    InitializeAtom(atom);
+    DisplayMessageAtom(atom, "Bonjour depuis C++ !");
+    DestroyAtom(atom);
+  }
+}
